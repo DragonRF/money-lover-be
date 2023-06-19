564e19a910e8083f48e1a760703e07991afd58bb
DragonRF <duonglong410@gmail.com>
2023-06-19 21:33:51 +0700

    add show wallet

--- src/wallets/wallet/wallet.service.ts
+++ src/wallets/wallet/wallet.service.ts
@@ -1,19 +1,33 @@
-import {Injectable} from "@nestjs/common";
-import {WalletDto} from "./wallet.dto";
-import {Wallet} from "../../entities/wallet.entity";
-import {User} from "../../entities/user.entity";
+// wallet.service.ts
+import { Injectable } from '@nestjs/common';
+import { WalletDto } from './wallet.dto';
+import { Wallet } from '../../entities/wallet.entity';
+import { User } from '../../entities/user.entity';
 
 @Injectable()
-export class WalletService{
+export class WalletService {
     async createWallet(walletDto: WalletDto, userId) {
-        let currentUser: User = await User.findOne({where: {id:userId}})
-        const {name, currency, initialBalance} = walletDto;
+        const currentUser: User = await User.findOne({ where: { id: userId } });
+        const { name, currency, initialBalance } = walletDto;
         const walletEntity: Wallet = Wallet.create();
         walletEntity.name = name;
         walletEntity.currency = currency;
         walletEntity.initialBalance = initialBalance;
-        walletEntity.user = currentUser
-        await walletEntity.save()
+        walletEntity.user = currentUser;
+        await walletEntity.save();
         return walletEntity;
+    }
+
+    async getWalletsByUserId(userId: number): Promise<Wallet[]> {
+        const currentUser: User = await User.findOne({
+            where: { id: userId },
+            relations: ['wallets'],
+            order: {
+                wallets: {
+                    initialBalance: "DESC"
+                }
+            }
+        });
+        return currentUser.wallets;
     }
 }

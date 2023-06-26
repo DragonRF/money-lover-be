8059283e5d859770bdfb8c764b2755eec6cbc13a
DragonRF <duonglong410@gmail.com>
2023-06-26 21:33:19 +0700

    add delete wallet

--- src/wallets/wallet/wallet.controller.ts
+++ src/wallets/wallet/wallet.controller.ts
@@ -1,13 +1,14 @@
 // wallet.controller.ts
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { WalletDto } from './wallet.dto';
-import { AuthGuard } from '../../auth/auth.guard';
-import { WalletService } from './wallet.service';
+import {Body, Controller, Delete, Get, Param, Post, Request, UseGuards} from '@nestjs/common';
+import {WalletDto} from './wallet.dto';
+import {AuthGuard} from '../../auth/auth.guard';
+import {WalletService} from './wallet.service';
 
 @UseGuards(AuthGuard)
 @Controller('wallets')
 export class WalletController {
-    constructor(private readonly walletService: WalletService) {}
+    constructor(private readonly walletService: WalletService) {
+    }
 
     @Post('/')
     async store(@Request() req): Promise<any> {
@@ -36,6 +37,24 @@ export class WalletController {
                 status: 'success',
                 data: wallets,
             };
+        } catch (error) {
+            return {
+                status: 'error',
+                message: error.message,
+            };
+        }
+    }
+
+    @Delete("/:id")
+    async deleteWallet(@Param() param: any) {
+        try {
+            const walletId = param.id
+            await this.walletService.deleteWalletById(walletId)
+            return {
+                status: 'success',
+                message: 'Delete wallet success!'
+            };
+
         } catch (error) {
             return {
                 status: 'error',
